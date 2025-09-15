d614bcdefbee21cb29f19d863c095d021ea0ec72
Bharath-05-ww <bk2689030@gmail.com>
2025-09-15 21:24:00 +0530

    Update Log.jsx

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -1,8 +1,8 @@
 export default function Log({ turns }) {
   return (
     <ol id="log">
-      {turns.map((turn) => (
-        <li key={`${turn.row}${turn.col}`}>
+      {turns.map((turn,index) => (
+        <li key={`${turn.square.row}-${turn.square.col}-${index}`}>
           {turn.player} Selected {turn.square.row},{turn.square.col}
         </li>
       ))}
